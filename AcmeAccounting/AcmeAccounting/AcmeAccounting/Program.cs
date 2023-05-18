50d6a260fd252b4d703260d37223bf0bcd931e92
MisterVal777 <MisterVal7@outlook.com>
2023-05-18 14:55:43 -0700

    Acme Accounting welcome message

---
+++ AcmeAccounting/AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Sytems \nRemember, we're \"accounting\" on you! ");
+        Console.ReadLine();
+    }
+}
+
