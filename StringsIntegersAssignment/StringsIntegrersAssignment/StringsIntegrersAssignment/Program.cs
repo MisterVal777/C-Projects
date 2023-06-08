95ce6252b5624971d86a6ed5a0af5279cda8fe43
MisterVal777 <MisterVal7@outlook.com>
2023-06-07 21:20:17 -0700

    Commit Strings and Integers Assignment

---
+++ StringsIntegersAssignment/StringsIntegrersAssignment/StringsIntegrersAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsIntegrersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                try
+                {
+                    List<int> list = new List<int>() { 20, 50, 100 };
+
+                    Console.WriteLine("Pick a number to divide against 20, 50, 100 ");
+                    int numberOne = Convert.ToInt32(Console.ReadLine());
+
+                    foreach (int number in list)
+                    {
+                        int numberTwo = number / numberOne;
+                        Console.WriteLine(numberTwo);
+                    }
+                    Console.ReadLine();
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please type a whole number");
+                    return;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Please dont divide by Zero");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }  
+        }
+    }
+}
