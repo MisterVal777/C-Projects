8f99a3d91fdfaf7e843515aeae5e9d78f32386b5
MisterVal777 <MisterVal7@outlook.com>
2023-06-11 21:15:13 -0700

    ..

--- MethodsAssignment/CallingMethodsAssignment/CallingMethodsAssignment/Math.cs
+++ MethodsAssignment/CallingMethodsAssignment/CallingMethodsAssignment/Math.cs
@@ -9,19 +9,19 @@ namespace CallingMethodsAssignment
     // 1.Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application
     public class Math
     {
-        public int add(int data, int value)
+        public int add(int data)
         {
-            return data + value;
+            return data + 7;
         }
 
-        public int subtract(int data, int value)
+        public int subtract(int data)
         {
-            return data - value;
+            return data - 4;
         }
 
         public int square(int data, int value)
         {
-            return data * value;
+            return data * data;
         }
     }
 }
--- MethodsAssignment/CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
+++ MethodsAssignment/CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -4,27 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CallingMethodsAssignment
+namespace MainMethodAssignment
 {
+
     class Program
     {
         public static int Data { get; set; }
         static void Main(string[] args)
 
         {
-        // In the Main() program, ask the user what number they want to do the math operations on.
-        Console.WriteLine("Enter a number this program will add 7, subtract 4, and than square that number... ");
-        var userValue = Convert.ToInt32(Console.ReadLine());
+            // In the Main() program, ask the user what number they want to do the math operations on.
+            Console.WriteLine("Enter a number this program will add 7, subtract 4, and than square that number... ");
+            var userValue = Convert.ToInt32(Console.ReadLine());
 
 
-        // 3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
-        var mathObject = new Math();
-        var result = mathObject.add(userValue, 7);
-        result = mathObject.subtract(result, 4  );
-        result = mathObject.square(result, result);
+            // 3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
+            var mathObject = new Math();
+            var result = mathObject.add(userValue);
+            Console.WriteLine(userValue + " + 7 is " + result);
+            Console.ReadLine();
+            result = mathObject.subtract(result);
+            Console.WriteLine("subtract 4 and you have " + result);
+            Console.ReadLine();
+            result = mathObject.square(result, result);
 
-        Console.WriteLine(result + " is the length of your next vacation stay in Avatar, thanks for playing!");
-        Console.ReadLine();
+            Console.WriteLine("Square this number and you get " + result + ", thanks for playing!");
+            Console.ReadLine();
 
         }
     }
