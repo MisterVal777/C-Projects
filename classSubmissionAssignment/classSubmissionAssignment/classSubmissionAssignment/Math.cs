a74bb5c19c4fca0a3ed75dbff466a929efb04a05
MisterVal777 <MisterVal7@outlook.com>
2023-06-14 12:33:04 -0700

    Commit Class submission assignment

---
+++ classSubmissionAssignment/classSubmissionAssignment/classSubmissionAssignment/Math.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSubmissionAssignment
+{
+    class Math
+    {
+        //1.  Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
+        public void divide(int num1)
+        {
+            int sum = num1 / 2;
+            Console.WriteLine(num1 + " divided by two is " + sum);
+        }
+        //4. Create a method with output parameters.
+        public void outNum(out int outNumber, out string outString)
+        {
+            outNumber = 10;
+            outString = "Hello World!";
+        }
+        //5. Overload a method.
+        public double area(double s)
+        {
+            double area = s * s;
+            return area;
+        }
+        public double area(double l, double b)
+        {
+            double area = l * b;
+            return area;
+        }
+
+        //6. Declare a class to be static
+        static class User
+        {
+            // Static Variables
+            public static string name;
+            public static string location;
+            public static int age;
+        }
+    }
+}
---
+++ classSubmissionAssignment/classSubmissionAssignment/classSubmissionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //2.In the Main() method, instantiate that class.
+            var mathObject = new Math();
+
+            //3. Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two.
+            Console.WriteLine("Please input a number to be divided by two.");
+            var number = Convert.ToInt32(Console.ReadLine());
+            mathObject.divide(number);
+            Console.ReadLine();
+
+        }
+    }
+}
