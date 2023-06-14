6804f7a7ae10455c0d79e8029961af092afba1d4
MisterVal777 <MisterVal7@outlook.com>
2023-06-14 09:34:45 -0700

    Commit Void method assignment

---
+++ VoidMethodAssignment/VoidMethodAssignment/VoidMethodAssignment/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Math
+    // 1.Create a class. In that class, create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen.
+    {
+        public void add(int num1, int num2)
+        {
+            int sum = num1 + 2;
+            Console.WriteLine(num2);
+        }        
+    }
+}
---
+++ VoidMethodAssignment/VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //2.In the Main() method of the console app, instantiate the class.
+            var mathObject = new Math();
+
+            //3. Call the method in the class, passing in two numbers.
+            mathObject.add(1, 5);
+            
+            //4. Call the method in the class, specifying the parameters by name.
+            void add(int num1, int num2, int sum)
+            {
+                Console.WriteLine(num1 + num2 + sum);               
+            }
+            Console.ReadLine();
+        }
+    }
+}
