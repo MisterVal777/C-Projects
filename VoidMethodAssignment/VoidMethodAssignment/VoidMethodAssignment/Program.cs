4b9fd9d198c67f077aff11806ad6fe4ce65424be
MisterVal777 <MisterVal7@outlook.com>
2023-06-14 11:05:37 -0700

    re-commit changes for named parameters step4.

--- VoidMethodAssignment/VoidMethodAssignment/VoidMethodAssignment/Program.cs
+++ VoidMethodAssignment/VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -16,13 +16,11 @@ namespace VoidMethodAssignment
 
             //3. Call the method in the class, passing in two numbers.
             mathObject.add(1, 5);
-            
+
             //4. Call the method in the class, specifying the parameters by name.
-            void add(int num1, int num2, int sum)
-            {
-                Console.WriteLine(num1 + num2 + sum);               
-            }
-            Console.ReadLine();
+            mathObject.add(num1: 1, num2: 5);
+
+            
         }
     }
 }
