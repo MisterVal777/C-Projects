f6862fefb6a3945ed0056769bbdfcd1bf039ea0b
MisterVal777 <MisterVal7@outlook.com>
2023-06-15 21:50:51 -0700

    commit partial assignment for help

---
+++ methodsAndObjectsAssignment/methodsAndObjectsAssignment/methodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsAndObjectsAssignment
+{
+    //1. Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
+    class Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            //2. Give this class a void method called SayName() that takes no parameters and simply writes the person's full name to the console in the format of: “Name: [full name]”.
+            void SayName()
+            {
+                string name = firstName + lastName;
+                Console.WriteLine("Name: " + name);
+            }
+        }
+        
+        
+        //public string firstName;
+        //public string lastName;
+    }
+}
---
+++ methodsAndObjectsAssignment/methodsAndObjectsAssignment/methodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //4. Inside of the Main method, instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
+        {
+            var personObject = new Person();
+
+        }
+    }
+}
