bf05fdee56d7e970d00f7683fdfab7bd3d14f1ba
MisterVal777 <MisterVal7@outlook.com>
2023-06-18 13:01:03 -0700

    Re-commit completed assignment

--- methodsAndObjectsAssignment/methodsAndObjectsAssignment/methodsAndObjectsAssignment/Employee.cs
+++ methodsAndObjectsAssignment/methodsAndObjectsAssignment/methodsAndObjectsAssignment/Employee.cs
@@ -9,9 +9,7 @@ namespace methodsAndObjectsAssignment
     //3. Create another class called Employee and have it inherit from the Person class. Give the Employee class a property called Id and have it be of data type int.
     public class Employee : Person
     {
-        public int Id(int a, int b)
-        {
-
-        }
+        public int Id { get; set; }
+   
     }
 }
--- methodsAndObjectsAssignment/methodsAndObjectsAssignment/methodsAndObjectsAssignment/Program.cs
+++ methodsAndObjectsAssignment/methodsAndObjectsAssignment/methodsAndObjectsAssignment/Program.cs
@@ -8,10 +8,17 @@ namespace methodsAndObjectsAssignment
 {
     class Program
     {
+        
         static void Main(string[] args)
         //4. Inside of the Main method, instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
         {
-            var personObject = new Person();
+
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Name";
+         //5. Call the superclass method SayName() on the Employee object.
+            employee.SayName();
+            Console.ReadLine();
 
         }
     }
