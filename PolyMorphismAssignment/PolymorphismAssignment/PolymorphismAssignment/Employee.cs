caecda04bb1fc86a8fe5e5d533fa5700185615cd
MisterVal777 <MisterVal7@outlook.com>
2023-06-19 10:27:26 -0700

    Commit Polymorphism assignment

---
+++ PolyMorphismAssignment/PolymorphismAssignment/PolymorphismAssignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    //2. Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("\"I\" is used first when naming the interface.");
+        }
+    }
+}
---
+++ PolyMorphismAssignment/PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Program
+
+        // Step 1. is in the "IQuittable" Interface.
+        // Step 2. is in the "Employee" class.
+        // Step 3. is in the main "Program".
+
+
+
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+
+            //3. Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.
+            IQuittable quit = new Employee();
+            quit.Quit();
+
+            Console.ReadLine();
+        }
+    
+    }
+}
