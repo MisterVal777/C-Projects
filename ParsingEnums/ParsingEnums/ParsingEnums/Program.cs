d0c228262200356fe977ef855a5cbe4b951d432c
MisterVal777 <MisterVal7@outlook.com>
2023-06-20 14:37:53 -0700

    re-commit

---
+++ ParsingEnums/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+
+    // 1. Create an enum for the days of the week.
+
+    // 2. Prompt the user to enter the current day of the week.
+
+    // 3. Assign the value to a variable of that enum data type you just created.
+
+    // 4. Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs.
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // 2. Prompt the user to enter the current day of the week.
+                Console.WriteLine("Please enter the current day of the week into the Console.");
+                string currentDay = Console.ReadLine();
+
+                var Day = System.Enum.Parse(typeof(WeekDays), currentDay);
+                //WeekDays parsedEnumValue = (WeekDays)System.Enum.Parse(typeof(WeekDays), currentDay);
+                Console.WriteLine( (WeekDays)System.Enum.Parse(typeof(WeekDays), currentDay));
+
+
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ParsingEnums/ParsingEnums/ParsingEnums/WeekDays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public class WeekDays
+    {
+        public Days Day { get; set; }
+        // 1. Create an enum for the days of the week.
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
