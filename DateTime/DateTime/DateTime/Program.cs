ded544d7b3391d708486f98a6b9ed5a8d788f232
MisterVal777 <MisterVal7@outlook.com>
2023-06-21 17:43:04 -0700

    commit DateTime Assignment

---
+++ DateTime/DateTime/DateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            //1.Prints the current date and time to the console.
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            //2.Asks the user for a number.
+            Console.WriteLine("input a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //3.Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+            Console.WriteLine("{0} + {1} = {2} ", now, number, now.AddHours(number));
+            Console.ReadLine();
+        }
+    }
+}
