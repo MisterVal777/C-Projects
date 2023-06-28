1bdbaf239385ed9013239cb4203b4e10d868f77f
MisterVal777 <MisterVal7@outlook.com>
2023-06-28 11:44:23 -0700

    Commit Student Data Base

---
+++ StudentDataBase/StudentDataBase/Program.cs
@@ -0,0 +1,23 @@
+﻿using StudentDataBase;
+using System;
+
+namespace StudentDataBase
+{
+    internal class Program
+    {
+        //Create a basic Entity Framework Code-First console application that creates a basic Student database and adds one student.
+
+        private static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Student added...");
+            Console.ReadLine();
+        }
+    }
+}
